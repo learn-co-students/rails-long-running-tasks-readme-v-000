a90d9007b0ce216b27c47925786fff245912fedb
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-23 19:52:23 +0000

    Done.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,15 @@
 class CustomersController < ApplicationController
+require 'csv'
 
   def index
     @customers = Customer.all
+  end
+
+  def upload
+    CSV.foreach(params[:leads].path, headers: true) do |lead|
+      Customer.create(email: leads[0], first_name: leads[1], last_name: leads[2] )
+    end
+  redirect_to customers_path
   end
 
 end
