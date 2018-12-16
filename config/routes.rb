465b05410be985e76338a2863df2ee24c02cb728
stazman <cadistasio@gmail.com>
2018-12-16 07:20:39 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :customers, only: [:index]
+  post 'customers/upload', to: 'customers#upload'
 end
 
-resources :customers, only: [:index]
-  post 'customers/upload', to: 'customers#upload'
+# resources :customers, only: [:index]
   
