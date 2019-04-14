3abbb5080fb42813b87bac8cd189fc5b88c99266
github username <github email address>
2019-04-14 19:19:51 -0400

    Done.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,14 @@
 class CustomersController < ApplicationController
+   require 'csv'
+
+   def upload
+    CSV.foreach(params[:leads].path, headers: true) do |lead|
+      binding.pry
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    end
+    redirect_to customers_path
+  end
+
 
   def index
     @customers = Customer.all
